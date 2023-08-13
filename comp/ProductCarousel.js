1e076605424bddf6b2e340021ab0cbc5d2882199
forgetdesenvolvimento <forgetdesenvolvimento@gmail.com>
2023-08-13 01:05:52 -0300

    navbar e carrosel

--- comp/ProductCarousel.js
+++ comp/ProductCarousel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import {] View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 
 const products = [
